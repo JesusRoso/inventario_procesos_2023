ac0caedc4a1e68a00402a4b01f0ac356b6baad2b
Jesus Roso <jr8campeones@gmail.com>
2023-03-21 20:56:52 -0500

    create new endpoints

--- src/main/java/com/procesos/inventario/controllers/UserController.java
+++ src/main/java/com/procesos/inventario/controllers/UserController.java
@@ -3,8 +3,12 @@ package com.procesos.inventario.controllers;
 import com.procesos.inventario.models.User;
 import com.procesos.inventario.services.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -13,11 +17,53 @@ public class UserController {
     private UserServiceImp userServiceImp;
 
     @GetMapping(value = "/user/{id}")
-    public User findUserById(@PathVariable Long id){
-        return userServiceImp.getUser(id);
+    public ResponseEntity findUserById(@PathVariable Long id){
+        Map response = new HashMap();
+        try{
+            return new ResponseEntity(userServiceImp.getUser(id), HttpStatus.OK);
+        }catch (Exception e){
+            response.put("status","404");
+            response.put("message", "user not found!");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @GetMapping(value = "/users")
+    public ResponseEntity findUsers(){
+        Map response = new HashMap();
+        try{
+            return new ResponseEntity(userServiceImp.allUsers(), HttpStatus.OK);
+        }catch (Exception e){
+            response.put("status","404");
+            response.put("message", "user not found!");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
     }
     @PostMapping(value = "/user")
-    public Boolean saveUser(@RequestBody User user){
-        return userServiceImp.createUser(user);
+    public ResponseEntity saveUser(@RequestBody User user){
+        Map response = new HashMap();
+        Boolean userResp = userServiceImp.createUser(user);
+        if(userResp){
+            response.put("status","201");
+            response.put("message", "user created successfully");
+            return new ResponseEntity(response, HttpStatus.CREATED);
+        }
+        response.put("status", "400");
+        response.put("message", "error creating user");
+        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+    }
+    @PutMapping(value = "{id}")
+    public ResponseEntity updateUser(Long id, @RequestBody User user){
+        Map response = new HashMap();
+        Boolean userResp = userServiceImp.updateUser(id, user);
+        if(userResp){
+            response.put("status","200");
+            response.put("message", "user updated successfully");
+            return new ResponseEntity(response, HttpStatus.CREATED);
+        }
+        response.put("status", "400");
+        response.put("message", "error updating user");
+        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
     }
 }
